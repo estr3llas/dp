276e294d4245cb410691bb1781753f1d63f39704
estr3llas <nao24nao69nao420@gmail.com>
2024-10-28 12:24:33 -0300

    Created helper to read the depex file and map it in memory

---
+++ dp/DpxFile/DpxFile.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Reflection;
+
+namespace dp.DpxFile;
+
+//
+// https://stackoverflow.com/questions/23588944/better-to-check-if-length-exceeds-max-path-or-catch-pathtoolongexception
+//
+public class PathHelper
+{
+    private static int MaxPathLength { get; set; }
+
+    static PathHelper()
+    {
+        //
+        // reflection
+        //
+        var maxPathField = typeof(Path).GetField(
+            "MaxPath", 
+            BindingFlags.Static | 
+            BindingFlags.GetField | 
+            BindingFlags.NonPublic
+            );
+
+        if (maxPathField == null)
+        {
+            MaxPathLength = 0;
+            return;
+        }
+
+        //
+        // invoke the field gettor, which returns 260
+        //
+        MaxPathLength = Convert.ToInt32(maxPathField.GetValue(null));
+
+        //
+        //the NUL terminator is part of MAX_PATH https://msdn.microsoft.com/en-us/library/aa365247.aspx#maxpath
+        //
+        MaxPathLength--;
+    }
+
+
+    public static bool IsPathWithinLimits(string fullPathAndFilename)
+    {
+        return fullPathAndFilename.Length <= MaxPathLength;
+    }
+
+}
+public class DpxFile : PathHelper
+{
+    public MemoryMappedFile DpxMapFile(string filename)
+    {
+
+        if (string.IsNullOrWhiteSpace(filename)) throw new ArgumentNullException($"[-] {filename} is null or whitespace.");
+
+        if (!IsPathWithinLimits(filename)) throw new PathTooLongException($"[-] {filename} is too long.");
+
+        //
+        // No need to check for the file's actual existence.
+        // For FileMode.Open, a FileNotFoundException exception is thrown if the file does not exist.
+        //
+        return MemoryMappedFile.CreateFromFile(filename, FileMode.Open);
+    }
+}
--- dp/DpxInstructionSet/DpxInstructionSet.cs
+++ dp/DpxInstructionSet/DpxInstructionSet.cs
@@ -1,101 +1,99 @@
-﻿namespace dp.DpxInstructionSet
+﻿namespace dp.DpxInstructionSet;
+public enum Opcodes : uint
 {
-    public enum Opcodes : uint
-    {
-        //
-        // Pushes a Boolean value onto the stack. 
-        // If the GUID is present in the handle database, then a TRUE is pushed onto the stack. 
-        // If the GUID is not present in the handle database, then a FALSE is pushed onto the stack. 
-        // The test for the GUID in the handle database may be performed with the Boot Service LocatePpi() .       
-        //
-        PUSH = 0x2,
+    //
+    // Pushes a Boolean value onto the stack. 
+    // If the GUID is present in the handle database, then a TRUE is pushed onto the stack. 
+    // If the GUID is not present in the handle database, then a FALSE is pushed onto the stack. 
+    // The test for the GUID in the handle database may be performed with the Boot Service LocatePpi() .       
+    //
+    PUSH = 0x2,
 
-        //
-        // Pops two Boolean operands off the stack, performs a Boolean AND operation between the two operands, and pushes the result back onto the stack.
-        //
-        AND = 0x3,
+    //
+    // Pops two Boolean operands off the stack, performs a Boolean AND operation between the two operands, and pushes the result back onto the stack.
+    //
+    AND = 0x3,
 
-        //
-        // Pops two Boolean operands off the stack, performs a Boolean OR operation between the two operands, and pushes the result back onto the stack.
-        //
-        OR = 0x4,
+    //
+    // Pops two Boolean operands off the stack, performs a Boolean OR operation between the two operands, and pushes the result back onto the stack.
+    //
+    OR = 0x4,
 
-        //
-        // Pops a Boolean operands off the stack, performs a Boolean NOT operation on the operand, and pushes the result back onto the stack.
-        //
-        NOT = 0x5,
+    //
+    // Pops a Boolean operands off the stack, performs a Boolean NOT operation on the operand, and pushes the result back onto the stack.
+    //
+    NOT = 0x5,
 
-        //
-        // Pushes a Boolean TRUE onto the stack.
-        //
-        TRUE = 0x6,
+    //
+    // Pushes a Boolean TRUE onto the stack.
+    //
+    TRUE = 0x6,
 
-        //
-        // Pushes a Boolean FALSE onto the stack.
-        // 
-        FALSE = 0x7,
+    //
+    // Pushes a Boolean FALSE onto the stack.
+    // 
+    FALSE = 0x7,
 
-        //
-        // Pops the final result of the dependency expression evaluation off the stack and exits the dependency expression evaluator.
-        // 
-        END = 0x8
-    }
+    //
+    // Pops the final result of the dependency expression evaluation off the stack and exits the dependency expression evaluator.
+    // 
+    END = 0x8
+}
 
-    internal class DpxInstructionSet
-    {
-        public List<Instruction> _instructions =
-        [
-            new Instruction
-            {
-                Mnemonic = MnemonicFromOpcode(Opcodes.PUSH), NumberOfOperands = 1
-            },
-            new Instruction
-            {
-                Mnemonic = MnemonicFromOpcode(Opcodes.AND), NumberOfOperands = 0
-            },
-            new Instruction
-            {
-                Mnemonic = MnemonicFromOpcode(Opcodes.OR), NumberOfOperands = 0
-            },
-            new Instruction
-            {
-                Mnemonic = MnemonicFromOpcode(Opcodes.NOT), NumberOfOperands = 0
-            },
-            new Instruction
-            {
-                Mnemonic = MnemonicFromOpcode(Opcodes.TRUE), NumberOfOperands = 0
-            },
-            new Instruction
-            {
-                Mnemonic = MnemonicFromOpcode(Opcodes.FALSE), NumberOfOperands = 0
-            },
-            new Instruction
-            {
-                Mnemonic = MnemonicFromOpcode(Opcodes.END), NumberOfOperands = 0
-            },
-
-        ];
-
-        public static string MnemonicFromOpcode(Opcodes opcode)
+internal class DpxInstructionSet
+{
+    public List<Instruction> _instructions =
+    [
+        new Instruction
         {
-            return opcode switch
-            {
-                Opcodes.PUSH => "PUSH",
-                Opcodes.AND => "AND",
-                Opcodes.OR => "OR",
-                Opcodes.NOT => "NOT",
-                Opcodes.TRUE => "TRUE",
-                Opcodes.FALSE => "FALSE",
-                Opcodes.END => "END",
-                _ => throw new ArgumentOutOfRangeException(nameof(opcode), $"[-] Invalid Opcode {opcode}")
-            };
-        }
+            Mnemonic = MnemonicFromOpcode(Opcodes.PUSH), NumberOfOperands = 1
+        },
+        new Instruction
+        {
+            Mnemonic = MnemonicFromOpcode(Opcodes.AND), NumberOfOperands = 0
+        },
+        new Instruction
+        {
+            Mnemonic = MnemonicFromOpcode(Opcodes.OR), NumberOfOperands = 0
+        },
+        new Instruction
+        {
+            Mnemonic = MnemonicFromOpcode(Opcodes.NOT), NumberOfOperands = 0
+        },
+        new Instruction
+        {
+            Mnemonic = MnemonicFromOpcode(Opcodes.TRUE), NumberOfOperands = 0
+        },
+        new Instruction
+        {
+            Mnemonic = MnemonicFromOpcode(Opcodes.FALSE), NumberOfOperands = 0
+        },
+        new Instruction
+        {
+            Mnemonic = MnemonicFromOpcode(Opcodes.END), NumberOfOperands = 0
+        },
 
-    }
+    ];
 
-    public class Instruction
+    public static string MnemonicFromOpcode(Opcodes opcode)
     {
-        public required string Mnemonic { get; init; }
-        public required uint NumberOfOperands { get; init; }
+        return opcode switch
+        {
+            Opcodes.PUSH => "PUSH",
+            Opcodes.AND => "AND",
+            Opcodes.OR => "OR",
+            Opcodes.NOT => "NOT",
+            Opcodes.TRUE => "TRUE",
+            Opcodes.FALSE => "FALSE",
+            Opcodes.END => "END",
+            _ => throw new ArgumentOutOfRangeException(nameof(opcode), $"[-] Invalid Opcode {opcode}")
+        };
     }
+
+}
+
+public class Instruction
+{
+    public required string Mnemonic { get; init; }
+    public required uint NumberOfOperands { get; init; }
 }
