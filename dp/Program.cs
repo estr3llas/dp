a6a4bba5d94bda3f6bd943ec1dec6b0a1d228fd1
estr3llas <nao24nao69nao420@gmail.com>
2024-10-29 15:00:33 -0300

    Implemented command line options handling

--- dp/Program.cs
+++ dp/Program.cs
@@ -7,18 +7,43 @@ using dp.DpxInstructionSet;
 using dp.DpxFileHandler;
 using dp.DpxDisassembler;
 
+using McMaster.Extensions.CommandLineUtils;
+
 namespace dp
 {
-    internal class Program
+
+    [Command(Name = "dp", Description = "Parse/patch UEFI module's dependency expressions")]
+    [HelpOption("-h")]
+    public class Program
     {
-        public static void Main(string[] args)
+        static Task<int> Main(string[] args) => CommandLineApplication.ExecuteAsync<Program>(args);
+
+        [Argument(0, Description = "The depex file to be processed.")]
+        private string filename { get; }
+
+        [Option("-d", Description = "Disassemble the given depex")]
+        public bool OptionDisassemble { get; }
+
+        private async Task<int> OnExecuteAsync(CommandLineApplication app, CancellationToken cancellationToken = default)
         {
-            var disasm = new DpxDisassembler.DpxDisassembler();
+            if (string.IsNullOrWhiteSpace(filename))
+            {
+                app.ShowHelp();
+                return 0;
+            }
 
-            var disassembled = disasm.DpxDisassembleBytecode(FileHandler.DpxReadFile(args[1]));
+            if (OptionDisassemble)
+            {
+                Console.WriteLine("[+] Disassembled output: \n");
 
-            Console.Write(disassembled);
+                var disasm = new DpxDisassembler.DpxDisassembler();
+                var disassembled = disasm.DpxDisassembleBytecode(FileHandler.DpxReadFile(filename));
+                Console.Write(disassembled);
+                return 0;
+            }
 
+            return 0;
         }
+
     }
 }
